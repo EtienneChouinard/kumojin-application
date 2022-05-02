d81b76a001cd86fe49d3fb6f3674a7e4353426de
EtienneChouinard <eti.chouinard@gmail.com>
2022-05-02 18:59:46 -0400

    my commit message

--- client/src/App.js
+++ client/src/App.js
@@ -14,9 +14,62 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{!data ? "Loading..." : data}</p>
+        <nav>
+          <a href="#">Accueil</a>
+          <a href="#">Liste événements</a>
+          <a href="#">Créer événement</a>
+        </nav>
       </header>
+      <main>
+        <div>
+          <p>{!data ? "Loading..." : data}</p>
+        </div>
+        <div>
+          <h2>Créer événement</h2>
+          <p>
+              Nom de l'événement: <input type='text' id='idNomEvenement'></input>
+          </p>
+          <p>
+              Date de l'événement: <input type='date' id='idDateEvenement'></input>
+          </p>
+          <p>
+              Fuseau horaire: 
+              <select id='idFuseauHoraire'>
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+              </select>
+          </p>
+          <p>
+              Heure de l'événement (heure locale <span id='idHeureLocaleType'></span>): <input type='date' id='idDateEvenement'></input>
+          </p>
+          <p>
+              Date et Heure de l'événement (heure de québec/EST): <span id='idDateEvenementQuebec'>-</span>
+          </p>
+          <p>
+            URL de l'événement: <input type='text' id='idDateEvenement'></input>
+          </p>
+          <button>Créer événement</button>
+        </div>
+        <div>
+          <h2>Liste Évenements</h2>
+        
+          <div>
+            <input type='text'></input>
+            <button>Rechercher</button>
+          </div>
+          <ul>
+            <li>...</li>
+            <li>...</li>
+            <li>...</li>
+          </ul>
+        </div>
+      </main>
+      <footer>
+
+        Créé par Étienne Chouinard 2022 - Tout droits réservés - Etc.
+      </footer>
     </div>
   );
 }
