33d3951a56e087ae01ca3cd9350d74adbfb4ad6f
EtienneChouinard <eti.chouinard@gmail.com>
2022-05-05 19:39:30 -0400

    Liste fonctionelle

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,146 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import { NavLink, Routes, Route } from 'react-router-dom';
+import Parser from 'html-react-parser';
 
-function App() {
+const App = () => (
+  <div className='app'>
+    
+    <Navigation />
+    <Main />
+    <Footer />
+  </div>
+);
+
+const Navigation = () => (
+  <header>
+    <nav>
+      <NavLink exact activeClassName="current" to='/'>Accueil</NavLink>
+      <NavLink exact activeClassName="current" to='/liste'>Liste événements</NavLink>
+      <NavLink exact activeClassName="current" to='/creer'>Créer événement</NavLink>
+      
+    </nav>
+  </header>
+);
+
+const Footer = () => (
+  
+  <footer>
+  
+    Créé par Étienne Chouinard 2022 - Tout droits réservés - Etc.
+  </footer>
+);
+
+const Home = () => (
+  <div className='home'>
+    <main>
+      <h2>Page d'accueil</h2>
+      <p>Veuillez utiliser la navigation pour voir les bonnes pages.</p>
+      <div id="prefooter"></div>
+    </main>
+  </div>
+);
+
+
+function Liste()
+{
+  function searchEvenements() 
+  {
+      var strEvenementRecherche = document.getElementById("recherheEvenement").value;
+
+      var allTags = document.getElementById('ulListeEvenements').getElementsByTagName("li");
+      for (var i = 0, len = allTags.length; i < len; i++) 
+      {
+          var labelInnerText = allTags[i].innerHTML;
+          
+          if (labelInnerText.includes(strEvenementRecherche))
+          {
+              allTags[i].style.display = "list-item";
+          }
+          else
+          {
+              allTags[i].style.display = "none";
+          }
+      }
+  }
+
+  function showEvenements()
+  {
+      var allTags = document.getElementById('ulListeEvenements').getElementsByTagName("li");
+      for (var i = 0, len = allTags.length; i < len; i++) 
+      {
+          allTags[i].style.display = "list-item";
+      }
+  }
+
+
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("/api/liste")
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  }, []);
+  
+  var stringListeEvenements = "";
+  //alert(data[0].nom);
+  /*
+  for (var i = 0, length = data.length; i < length; i++)
+  {
+    
+    var evenementNom = data[i].nom;
+    var evenementHeureQuebec = data[i].heure;
+    var evenementDate = data[i].date;
+    var evenementHeureLocale = data[i].heure;
+    var evenementTimezone = data[i].timezone;
+    //var evenementOffset = data[i].offset;
+    var evenementDescription = data[i].description;
+    var evenementsiteWeb = data[i].siteWeb;
+
+    stringListeEvenements = stringListeEvenements + "<li><div class='evenement'>"
+    stringListeEvenements = stringListeEvenements + "<p>Nom: " + evenementNom + "</p>"
+    stringListeEvenements = stringListeEvenements + "<p>Heure (Québec): " + evenementHeureQuebec + "</p>"
+    stringListeEvenements = stringListeEvenements + "<p>Date: " + evenementDate + "</p>"
+    stringListeEvenements = stringListeEvenements + "<p>Heure (Locale): " + evenementHeureLocale + "</p>"
+    stringListeEvenements = stringListeEvenements + "<p>Fuseau horaire local: " + evenementTimezone + "</p>"
+    stringListeEvenements = stringListeEvenements + "<p>Description: " + evenementDescription + "</p>"
+    stringListeEvenements = stringListeEvenements + "<p>Site web: " + evenementsiteWeb + "</p>"
+    
+    stringListeEvenements = stringListeEvenements + "</ul></li>"
+    
+    //console.log("Nom: ", evenements[i].nom);
+    //console.log("Date: ", evenements[i].date);
+  }
+  */
+  //alert(stringListeEvenements);
+  //alert(data[0].nom);
+
+  //<p>{!stringListeEvenements ? "Loading..." : stringListeEvenements}</p>
+  //
+  var AAA = (<div className='liste'>
+  <main>
+    <h2>Liste Évenements</h2>
+    
+    <div>
+      <input type='text' id='recherheEvenement'></input>
+      <button id="btn_rechercher" onClick={searchEvenements}>Rechercher</button>
+      <button id="btn_annulerRecherche" onClick={showEvenements}>Annuler</button>
+    </div>
+    
+    <ul id='ulListeEvenements'>
+      {Parser(stringListeEvenements)}
+      
+    </ul>
+    <div id="prefooter"></div>
+  </main>
+</div>);
+
+  return (AAA);
+}
+
+function Creer()
+{
+  /*
   const [data, setData] = React.useState(null);
 
   React.useEffect(() => {
@@ -10,22 +148,18 @@ function App() {
       .then((res) => res.json())
       .then((data) => setData(data.message));
   }, []);
+*/
+  //<p>{!stringListeEvenements ? "Loading..." : stringListeEvenements}</p>
+  
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <nav>
-          <a href="#">Accueil</a>
-          <a href="#">Liste événements</a>
-          <a href="#">Créer événement</a>
-        </nav>
-      </header>
+    <div className='creer'>
       <main>
         <div>
-          <p>{!data ? "Loading..." : data}</p>
+          
         </div>
-        <div>
-          <h2>Créer événement</h2>
+        <h2>Créer événement</h2>
+        <form action="POST" id="CreerEvenement">
           <p>
               Nom de l'événement: <input type='text' id='idNomEvenement'></input>
           </p>
@@ -51,27 +185,20 @@ function App() {
             URL de l'événement: <input type='text' id='idDateEvenement'></input>
           </p>
           <button>Créer événement</button>
-        </div>
-        <div>
-          <h2>Liste Évenements</h2>
-        
-          <div>
-            <input type='text'></input>
-            <button>Rechercher</button>
-          </div>
-          <ul>
-            <li>...</li>
-            <li>...</li>
-            <li>...</li>
-          </ul>
-        </div>
+        </form>
+        <div id="prefooter"></div>
       </main>
-      <footer>
-
-        Créé par Étienne Chouinard 2022 - Tout droits réservés - Etc.
-      </footer>
     </div>
   );
 }
+const Main = () => (
+  <Routes>
+    <Route exact path='/' element={<Home/>}></Route>
+    <Route exact path='/liste' element={Liste()}></Route>
+    <Route exact path='/creer' element={Creer()}></Route>
+  </Routes>
+);
+
+
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -1,18 +1,197 @@
 // server/index.js
 const path = require('path');
 const express = require("express");
+const timezones = require("./timezone.JSON");
+const fs = require("fs");
+const { stringify } = require('querystring');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+function jsonReader(filePath, cb) 
+{
+  fs.readFile(filePath, "utf8",(err, fileData) => 
+  {
+    if (err) 
+    {
+      return cb && cb(err);
+    }
+    try 
+    {
+      const object = JSON.parse(fileData);
+      return cb && cb(null, object);
+    } 
+    catch (err) 
+    {
+      return cb && cb(err);
+    }
+  });
+}
+
   // Have Node serve the files for our built React app
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
-// Handle GET requests to /api route
-app.get("/api", (req, res) => {
-  res.json({ message: "Hello from server!" });
+app.get("/api/liste", (req, res) => 
+{
+  //Read
+  jsonReader("./server/evenements.JSON", (err, evenements) => 
+  {
+    if (err) 
+    {
+      console.log(err);
+      return;
+    }
+    try
+    {
+      /*var stringListeEvenements = "";
+      for (var i = 0, length = evenements.length; i < length; i++)
+      {
+        stringListeEvenements = stringListeEvenements + "<li>" + evenements[i].nom + "</li>"
+        
+        console.log("Nom: ", evenements[i].nom);
+        //console.log("Date: ", evenements[i].date);
+      }
+      console.log(evenements);*/
+      res.json({ message: evenements });
+    }
+    catch (err)
+    {
+      console.log("Error parsing JSON", err);
+    }
+    
+  });
+})
+
+app.get("/api", (req, res) => 
+{
+  //Read
+  jsonReader("./server/evenements.JSON", (err, evenements) => 
+  {
+    if (err) 
+    {
+      console.log(err);
+      return;
+    }
+    try
+    {
+      var stringListeEvenements = "";
+      for (var i = 0, length = evenements.length; i < length; i++)
+      {
+        stringListeEvenements = stringListeEvenements + "<li>" + evenements[i].nom + "</li>"
+        
+        console.log("Nom: ", evenements[i].nom);
+        //console.log("Date: ", evenements[i].date);
+      }
+      console.log(stringListeEvenements);
+      res.json({ message: "evenements" });
+    }
+    catch (err)
+    {
+      console.log("Error parsing JSON", err);
+    }
+    
+  });
+
 });
+
+// Handle GET requests to /api route
+app.get("/api/Test", (req, res) => 
+{
+  
+  const newEvenementTest =
+  {
+    nom: "Festival d'automne de Québec",
+    date: "2022/07/07",
+    heure: "20:00",
+    timezone: "Eastern Standard Time",
+    offset: -5,
+    description: "Insert description here"
+  }
+
+  //Read
+  jsonReader("./server/evenements.JSON", (err, evenements) => 
+  {
+    if (err) 
+    {
+      console.log(err);
+      return;
+    }
+    try
+    {
+      var stringListeEvenements = "";
+      for (var i = 0, length = evenements.length; i < length; i++)
+      {
+        stringListeEvenements = stringListeEvenements + "<li>" + evenements[i].nom + "</li>"
+        
+        console.log("Nom: ", evenements[i].nom);
+        //console.log("Date: ", evenements[i].date);
+      }
+      console.log(stringListeEvenements);
+      res.json({ message: "stringListeEvenements" });
+    }
+    catch (err)
+    {
+      console.log("Error parsing JSON", err);
+    }
+    
+  });
+  
+  
+
+  //Append
+  /*
+  jsonReader("./server/evenements.JSON", (err, evenements) => 
+  {
+    if (err) 
+    {
+      console.log(err);
+      return;
+    }
+    try
+    {
+      evenements.push(newEvenementTest);    
+      fs.writeFile("./server/evenements.JSON", JSON.stringify(evenements, null, 2), function(err)
+      {
+        if (err) throw err;
+        console.log('The "data to append" was appended to file!');
+      });
+    }
+    catch (err)
+    {
+      console.log("Error parsing JSON", err);
+    }
+  })
+  */
+  /*
+  const customer = 
+  {
+    name: "Newbie Corp2.",
+    order_count: 3,
+    address: "Po Box City+"
+  };
+  const customer2 = 
+  {
+    name: "Newbie Corp3.",
+    order_count: 4,
+    address: "Po Box City++"
+  };
+  
+
+  const jsonString2 = JSON.stringify(customer, null, 2)
+  fs.writeFile('./server/test.json', jsonString2, err => 
+  {
+    if (err) 
+    {
+        console.log('Error writing file', err)
+    } else 
+    {
+        console.log('Successfully wrote file')
+    }
+  })
+  */
+});
+
 
 // All other GET requests not handled before will return our React app
 app.get('*', (req, res) => {
